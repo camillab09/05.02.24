92b8bfa0c2d95b3bd8c1e688396f91debbaf9a53
Camilla <camillab09.kodehode@gmail.com>
2024-02-05 11:48:33 +0100

    React project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>world</p>
+      <br />
+      <img src="./images/cartwheel.gif" alt="bear cartwheeling" />
+    </>
+  );
+}
+
+export default App;
